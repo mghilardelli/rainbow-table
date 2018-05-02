7fdab9bd69750a4790d4685105f55011694d62de
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2018-05-02 12:47:32 +0200

    generated passwords

--- src/ch/fhnw/Main.java
+++ src/ch/fhnw/Main.java
@@ -2,7 +2,41 @@ package ch.fhnw;
 
 public class Main {
 
+    static int PASSWORD_COUNT = 2000;
+
+    static String[] passwords = new String[PASSWORD_COUNT];
+
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < 2000; i++) {
+            String password = "0000000";
+            for (int j = 0; j < i; j++) {
+                password = generatePassword(password, 6);
+            }
+            passwords[i] = password;
+        }
+
+
+        for (String s : passwords)
+            System.out.println(s);
+
+    }
+
+    private static String generatePassword(String s, int pos) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        int i = (int) stringBuilder.charAt(pos);
+
+        if (i >= 48 && i <= 56) {
+            stringBuilder.setCharAt(pos, (char) (i + 1));
+        } else if (i == 57) {
+            stringBuilder.setCharAt(pos, 'a');
+        } else if (i >= 97 && i <= 121) {
+            stringBuilder.setCharAt(pos, (char) (i + 1));
+        } else if (i == 122) {
+            String received = generatePassword(stringBuilder.toString(), (pos - 1));
+            stringBuilder.delete(0, 7).append(received);
+            stringBuilder.setCharAt(pos, '0');
+        }
+
+        return stringBuilder.toString();
     }
 }
